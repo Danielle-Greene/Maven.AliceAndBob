26531e1c48f2da11ef543c5538186aa90fbc56eb
Danielle <danielle@Zipcoders-MacBook-Pro-8.local>
2023-06-29 19:50:38 -0400

    I have made changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,30 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args ){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello, what is your Name?");
+
+        String response;
+        response = scanner.nextLine();
+
+        String alice = "Alice";
+        String bob = "Bob";
+
+         if(alice.equals(response))
+        {
+            System.out.println("Hello Alice!");
+        }
+
+        if(bob.equals(response)) {
+            System.out.println("Hello Bob!");
+        }
+
 
     }
 }
